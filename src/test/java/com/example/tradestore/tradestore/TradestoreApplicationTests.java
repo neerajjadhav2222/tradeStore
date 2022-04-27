8db6144c598aea20731fd51f28a2cc2db5d8e2d1
neerajjadhav2222 <neerajjadhav2222@gmail.com>
2022-04-28 00:28:39 +0530

    initial setup

---
+++ src/test/java/com/example/tradestore/tradestore/TradestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradestore.tradestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
